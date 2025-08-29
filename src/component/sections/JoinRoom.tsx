98a5810431bd228d23c6b34898aab9d31f2612d0
Ezekiel <ezekielgbustamante@iskolarngbayan.pup.edu.ph>
2025-08-29 19:15:10 +0800

    feat: :D
    Automated commit to sync local changes.
    Timestamp: 2025-08-29 11:15:10 UTC
    Summary:
    - Added: 0
    - Modified: 1
    - Deleted: 0
    - Renamed: 0
    Changed files:
    - src/component/sections/JoinRoom.tsx

--- src/component/sections/JoinRoom.tsx
+++ src/component/sections/JoinRoom.tsx
@@ -1,10 +1,32 @@
+import { Playfair_Display } from "next/font/google";
+
+const playfair = Playfair_Display({
+    subsets: ["latin"],
+    display: "swap",
+    weight: ["400", "700"],
+});
+
 export default function Join() {
     return (
-        <div className="flex items-center justify-center min-h-screen">
-            <div className="flex items-center justify-center bg-white py-20 w-100 h-auto rounded-xl shadow-sm">
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="flex flex-col items-center justify-center bg-white py-20 w-100 h-auto rounded-xl shadow-sm">
                 <span>
-                    <img src='/brainlink.svg' />
+                    <img src='/brainlink.svg' draggable='false'/>
                 </span>
+
+                <h1 className={`${playfair.className}`}>Join Study Room</h1>
+                <p className="text-[#666666]">Enter a class code to join or create a new room</p>
+
+                <label className="flex justify-start mb-2">Class Code</label>
+                <input 
+                    type="text" 
+                    placeholder="Enter class code" 
+                    className="border border-gray-300 rounded-md px-3 py-2 w-64 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+
+                <button type="submit">Join Room
+                    
+                </button>
             </div>
         </div>
     );
