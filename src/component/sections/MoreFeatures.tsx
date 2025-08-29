ff4db17f62a2b97b2e3b53bb04b000016a485559
Ezekiel <ezekielgbustamante@iskolarngbayan.pup.edu.ph>
2025-08-29 16:18:24 +0800

    chore(BrainLink-AI): automated sync - 1 modified
    Automated commit to sync local changes.
    Timestamp: 2025-08-29 08:18:24 UTC
    Summary:
    - Added: 0
    - Modified: 1
    - Deleted: 0
    - Renamed: 0
    Changed files:
    - src/component/sections/MoreFeatures.tsx

--- src/component/sections/MoreFeatures.tsx
+++ src/component/sections/MoreFeatures.tsx
@@ -12,16 +12,22 @@ const playfair = Playfair_Display({
 });
 
 export default function MoreFeats() {
+
+    const cards = 'bg-white p-10 rounded-xl shadow-sm flex items-center justify-center flex-col'
+
     return (
         <div className="flex flex-col min-h-screen items-center justify-center gap-3">
             <h1 className={`${playfair.className} text-4xl font-bold text-[#333333]`}>More Amazing Features</h1>
 
             <span className="px-15 py-1 bg-gradient-to-r from-[#B32725] via-[#CA6C5B] to-[#E2B492] rounded mb-10"></span>
 
-            <div className="grid grid-cols-4 gap-4 max-w-3xl w-full">
-                <div className="bg-white p-10 rounded-xl shadow-sm flex items-center justify-center">
-                    <span className="bg-gradient-to-br from-[#B62424] to-[#D53838]"><ChatBubbleOutlineOutlinedIcon /></span>
-                    
+            <div className="grid grid-cols-4 gap-4 max-w-4xl w-full mx-auto">
+                <div className={`${cards}`}>
+                    <span className="bg-gradient-to-br from-[#B62424] to-[#D53838] p-2 rounded-xl"><ChatBubbleOutlineOutlinedIcon className="text-[#fff]" fontSize="medium"/></span>
+
+                    <h1 className={`${playfair.className} `}>Smart Chat System</h1>
+
+                    <p>Enhanced messaging with AI-powered features</p>
                 </div>
             </div>
 
