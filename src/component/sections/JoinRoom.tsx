9d4961d1886a4292f67b046b41f74f72458a53ff
Ezekiel <ezekielgbustamante@iskolarngbayan.pup.edu.ph>
2025-08-29 19:10:10 +0800

    feat: :D
    Automated commit to sync local changes.
    Timestamp: 2025-08-29 11:10:10 UTC
    Summary:
    - Added: 0
    - Modified: 1
    - Deleted: 0
    - Renamed: 0
    Changed files:
    - src/component/sections/JoinRoom.tsx

--- src/component/sections/JoinRoom.tsx
+++ src/component/sections/JoinRoom.tsx
@@ -1,7 +1,11 @@
 export default function Join() {
     return (
-        <div>
-            
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="flex items-center justify-center bg-white py-20 w-100 h-auto rounded-xl shadow-sm">
+                <span>
+                    <img src='/brainlink.svg' />
+                </span>
+            </div>
         </div>
     );
 }
