49f622c0e681a8e129119395e9b92ccac39a15de
Ezekiel <ezekielgbustamante@iskolarngbayan.pup.edu.ph>
2025-08-31 15:04:03 +0800

    feat: :D
    Automated commit to sync local changes.
    Timestamp: 2025-08-31 07:04:03 UTC
    Summary:
    - Added: 5
    - Modified: 1
    - Deleted: 2
    - Renamed: 0
    Changed files:
    - src/app/SignIn/page.tsx
    - src/app/Signup/page.tsx
    - src/components/login-form.tsx
    - src/components/sections/Topbar.tsx
    - src/components/ui/button.tsx
    - src/components/ui/card.tsx
    - src/components/ui/input.tsx
    - src/components/ui/label.tsx

--- src/components/sections/Topbar.tsx
+++ src/components/sections/Topbar.tsx
@@ -1,8 +1,12 @@
 "use client";
+
 import { useTabs } from "../context/TabsContext";
+import { useRouter } from "next/navigation";
+
 
 export default function Topbar() {
     const { active, setActive } = useTabs();
+    const router = useRouter();
 
     const baseLink =
         "relative inline-flex items-center px-1 py-2 font-semibold text-[#666666] hover:text-[#B32222] transition-colors duration-400 focus:outline-none after:absolute after:left-1/2 after:-translate-x-1/2 after:-bottom-2 after:h-2 after:w-2 after:rounded-full after:bg-[#B32222] after:opacity-0";
@@ -48,7 +52,9 @@ export default function Topbar() {
                 </nav>
 
                 <div className="flex justify-end gap-3 mr-5">
-                        <button className="px-4 py-2 rounded-xl hover:bg-[#F2DFDB] transition-colors duration-400 font-semibold cursor-pointer">Sign In</button>
+                        <button 
+                        onClick={() => router.push("/SignIn")}
+                        className="px-4 py-2 rounded-xl hover:bg-[#F2DFDB] transition-colors duration-400 font-semibold cursor-pointer">Sign In</button>
                         <button className="px-4 py-2 rounded-xl bg-[#B32222] text-white text-sm font-semibold shadow-sm transition-all duration-500 ease-out hover:shadow-lg hover:drop-shadow-[0_6px_12px_rgba(179,34,34,0.25)] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-[#B32222]/40 cursor-pointer">Sign Up</button>
                 </div>
 
