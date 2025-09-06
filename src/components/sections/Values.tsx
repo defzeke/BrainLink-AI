7fe9e1b6c2d2e19d4093e97e036c6916625ea9a6
Ezekiel <ezekielgbustamante@iskolarngbayan.pup.edu.ph>
2025-09-06 16:49:43 +0800

    feat: :D
    Automated commit to sync local changes.
    Timestamp: 2025-09-06 08:49:42 UTC
    Summary:
    - Added: 0
    - Modified: 1
    - Deleted: 0
    - Renamed: 0
    Changed files:
    - src/components/sections/Values.tsx

--- src/components/sections/Values.tsx
+++ src/components/sections/Values.tsx
@@ -34,7 +34,7 @@ export default function Values() {
 
     return (
         <div className="flex flex-col min-h-screen items-center justify-center gap-3">
-            <h1 ref={valuesRef} className={`${playfair.className} text-4xl font-bold text-[#333333]`}>Our Values</h1>
+            <h1 ref={valuesRef} className={`${playfair.className} text-4xl font-bold text-[#333333] ${isVisible ? "fade-in-up" : "opacity-0"}`}>Our Values</h1>
 
             <span className="px-15 py-1 bg-gradient-to-r from-[#B32725] via-[#CA6C5B] to-[#E2B492] rounded mb-10"></span>
 
