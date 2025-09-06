be9c628687a274462093a896c6a2f89f3fe83287
Ezekiel <ezekielgbustamante@iskolarngbayan.pup.edu.ph>
2025-09-06 12:14:53 +0800

    feat: :D
    Automated commit to sync local changes.
    Timestamp: 2025-09-06 04:14:53 UTC
    Summary:
    - Added: 1
    - Modified: 1
    - Deleted: 0
    - Renamed: 0
    Changed files:
    - public/signin.jpg
    - src/components/login-form.tsx

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -88,7 +88,7 @@ export function LoginForm({
           </form>
           <div className="bg-muted relative hidden md:block">
             <img
-              src="/placeholder.svg"
+              src="/signin.jpg"
               alt="image"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
