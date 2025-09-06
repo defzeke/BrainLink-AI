663997ce68ae7367ae58d9006df8e3b26bb5b1d3
Ezekiel <ezekielgbustamante@iskolarngbayan.pup.edu.ph>
2025-09-06 17:05:43 +0800

    feat: :D
    Automated commit to sync local changes.
    Timestamp: 2025-09-06 09:05:42 UTC
    Summary:
    - Added: 0
    - Modified: 1
    - Deleted: 0
    - Renamed: 0
    Changed files:
    - src/components/sections/MainFeats.tsx

--- src/components/sections/MainFeats.tsx
+++ src/components/sections/MainFeats.tsx
@@ -98,7 +98,7 @@ export default function Features() {
                     </div>
                 </div>
 
-                <div ref={featRef} className={`${cards} bg-gradient-to-r from-[#CBEDCB] to-[#E0F5E0] outline-[#9CDC9C]`}>
+                <div className={`${cards} bg-gradient-to-r from-[#CBEDCB] to-[#E0F5E0] outline-[#9CDC9C]`}>
                     <div className="w-16 h-16 bg-[#C7C4AA] rounded-xl flex items-center justify-center">
                         <ElectricBoltOutlinedIcon className="text-[#B12020]" fontSize="large"/>
                     </div>
