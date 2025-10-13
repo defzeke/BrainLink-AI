a59fc61acf1a809fc0a7a5e629b99b6aedc43163
ezekielgbustamante@iskolarngbayan.pup.edu.ph <ezekielgbustamante@iskolarngbayan.pup.edu.ph>
2025-10-12 22:46:03 -0800

    feat: :D
    Automated commit to sync local changes.
    Timestamp: 2025-10-13 06:46:03 UTC
    Summary:
    - Added: 0
    - Modified: 1
    - Deleted: 0
    - Renamed: 0
    Changed files:
    - src/components/login-form.tsx

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -11,6 +11,7 @@ export function LoginForm({
 }: React.ComponentProps<"div">) {
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <span className="bg-red-400"><ClearIcon /></span>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
           <form className="p-6 md:p-8">
