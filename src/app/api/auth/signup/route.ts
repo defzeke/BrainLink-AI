96d8c8a356a6a6d90248e687ac8c965c0948c719
ezekielgbustamante@iskolarngbayan.pup.edu.ph <ezekielgbustamante@iskolarngbayan.pup.edu.ph>
2025-10-15 18:44:18 -0800

    feat: :D
    Automated commit to sync local changes.
    Timestamp: 2025-10-16 02:44:18 UTC
    Summary:
    - Added: 0
    - Modified: 1
    - Deleted: 0
    - Renamed: 0
    Changed files:
    - src/app/api/auth/signup/route.ts

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -2,5 +2,11 @@ import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-    process.env.
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
+
+export async function POST(request: Request) {
+    try
+    
+}
