9e1780b7248c749b15fe1a435b2134a3eea1fdfa
ezekielgbustamante@iskolarngbayan.pup.edu.ph <ezekielgbustamante@iskolarngbayan.pup.edu.ph>
2025-10-16 08:14:08 -0800

    feat: :D
    Automated commit to sync local changes.
    Timestamp: 2025-10-16 16:14:08 UTC
    Summary:
    - Added: 0
    - Modified: 1
    - Deleted: 0
    - Renamed: 0
    Changed files:
    - src/components/sections/JoinRoom.tsx

--- src/components/sections/JoinRoom.tsx
+++ src/components/sections/JoinRoom.tsx
@@ -22,7 +22,7 @@ export default function Join() {
                     <input
                         type="text"
                         placeholder="Enter class code (e.g. ABC123)"
-                        className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm md:text-base focus:bg-[#fff]"
+                        className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-[#E79A9A] text-sm md:text-base"
                     />
                 </div>
                 <button
