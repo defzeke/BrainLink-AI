6d6adbc4755636a178ec730c6fdf14759ad8a797
ezekielgbustamante@iskolarngbayan.pup.edu.ph <ezekielgbustamante@iskolarngbayan.pup.edu.ph>
2025-10-15 17:57:39 -0800

    feat: enhance spacing in Home component layout

--- src/components/tabs/Home.tsx
+++ src/components/tabs/Home.tsx
@@ -5,7 +5,7 @@ import Cta from '../sections/CTA';
 
 export default function Home() {    
     return (
-        <div className='pt-16 px-4 sm:px-6 lg:px-8'>
+        <div className='pt-16 px-4 sm:px-6 lg:px-8 space-y-16 md:space-y-24 lg:space-y-32'>
             <Hero />
             <Benefits />
             <Cta />
