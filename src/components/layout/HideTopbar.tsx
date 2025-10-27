af319b3247aeedac6086cbe163f7d8cfeb0ff659
ezekielgbustamante@iskolarngbayan.pup.edu.ph <ezekielgbustamante@iskolarngbayan.pup.edu.ph>
2025-10-26 20:55:48 -0800

    feat: :D
    Automated commit to sync local changes.
    Timestamp: 2025-10-27 04:55:48 UTC
    Summary:
    - Added: 0
    - Modified: 1
    - Deleted: 0
    - Renamed: 0
    Changed files:
    - src/components/layout/HideTopbar.tsx

--- src/components/layout/HideTopbar.tsx
+++ src/components/layout/HideTopbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { usePathname } from "next/navigation";
-import Topbar from "./sections/Topbar";
+import Topbar from "../sections/Topbar";
 
 export default function HideTopbar({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
